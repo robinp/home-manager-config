864ff70b1d5e637fc6d6ca4d8150a964bc127917
Robin Palotai <palotai.robin@gmail.com>
2019-08-19 15:41:38 +0200

    Initial config.

---
+++ home.nix
@@ -0,0 +1,151 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.sessionVariables = {
+    EDITOR = "vim";
+    TERM = "xterm-256color";
+  };
+
+  home.packages =
+    with pkgs;
+    with rec {
+      customPlugins = {
+        ghcid-quickfix = vimUtils.buildVimPlugin {
+          name = "ghcid-quickfix";
+          src = fetchFromGitHub {
+            owner = "aiya000";
+            repo = "vim-ghcid-quickfix";
+            rev = "07c9b377f1dfadb3e90e42011e7c006e3713c9e1";
+            sha256 = "0askza9v60qvm7gjkcd8rg5v89nfy2fnlwbk7qd2b5dz83g3lqjj";
+          };
+        };
+        vim-wordy = vimUtils.buildVimPlugin {
+          name = "vim-wordy";
+          src = fetchFromGitHub {
+            #owner = "reedes";
+            owner = "robinp";  # until PR is merged
+            repo = "vim-wordy";
+            rev = "4e097f5552731229cbda3ed7b55004b56c2b84f4";
+            sha256 = "1c235faydn95s4nwa4in6ag58r00nclqxncrlvby2kcpm8l2r0kz";
+          };
+        };
+      };
+      myvim = vim_configurable.customize {
+        name = "vim";
+        vimrcConfig.packages.myVimPackage = with vimPlugins; {
+          start = [
+            sensible
+            vim-unimpaired  # ?
+
+            # To highlight eol-spaces etc.
+            vim-better-whitespace
+
+            bufexplorer
+            gitgutter
+
+            molokai
+            airline vim-airline-themes
+
+            # Haskell
+            customPlugins.ghcid-quickfix
+
+            # Writing
+
+            # :Wordy weak
+            # :NoWordy
+            # :NextWordy
+            # ]s [s to go through problematic
+            customPlugins.vim-wordy
+
+            # TODO(robinp):
+            # fzf tabular repeat supertab easymotion tmux-navigator fugitive surround tmuxline
+          ];
+        };
+        vimrcConfig.customRC = ''
+          set nocompatible
+          set expandtab
+          set number
+          set tabstop=2
+          set shiftwidth=2
+
+          set colorcolumn=80
+
+          colorscheme molokai
+          let g:airline_powerline_fonts = 1
+
+          let g:wordy_spell_dir = '/home/ron/wordy'
+        '';
+      };
+    };
+    [
+      brave
+
+      myvim
+
+      fira-code  # Note: fire-code supports powerline (/airline)
+
+      thefuck
+      multitail
+      curl wget nmap
+      xscreensaver
+      youtube-dl
+      # xdotool
+    ];
+
+  home.file.".bashrc".text = ''
+      eval $(thefuck --alias)
+  '';
+
+  programs.git = {
+    enable = true;
+    userName = "Robin Palotai";
+    userEmail = "palotai.robin@gmail.com";
+    aliases = {
+      co = "checkout";
+      st = "status";
+    };
+  };
+
+  programs.urxvt = {
+    enable = true;
+    extraConfig = {
+      background = "black";
+      foreground = "white";
+    };
+    fonts = [
+      "xft:Fira Code:size=10"
+    ];
+    scroll.bar.enable = false;
+    shading = 20;
+    transparent = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    terminal = "screen-256color";
+    extraConfig = ''
+      set -g status-bg colour237
+      set -g status-bg colour249
+    '';
+  };
+
+  programs.taskwarrior = {
+    enable = true;
+  };
+
+  services.redshift = {
+    enable = true;
+    latitude = "47.49801";
+    longitude = "19.03991";
+  };
+
+  services.random-background = {
+    enable = false;
+    imageDirectory = "%h/backgrounds";
+    interval = "1h";
+  };
+}
